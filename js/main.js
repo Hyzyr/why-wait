3fb7b7976754b3b8baef0314593eadea9d8eb5fb
Ahmet <ahmetcanlyev@gmail.com>
2024-07-05 18:25:51 +0500

    project join login pages

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-var menu = document.getElementById('menu');
-var menuBtn = document.getElementById('menuBtn');
+var menu = document.getElementById("menu");
+var menuBtn = document.getElementById("menuBtn");
 var body = document.body;
 
 const closeMenu = () => {
-  menu.classList.remove('active');
-  menuBtn.classList.remove('active');
-  body.classList.remove('active');
+  menu.classList.remove("active");
+  menuBtn.classList.remove("active");
+  body.classList.remove("active");
 };
 
 menuBtn.onclick = function () {
-  menu.classList.toggle('active');
-  menuBtn.classList.toggle('active');
-  body.classList.toggle('active');
+  menu.classList.toggle("active");
+  menuBtn.classList.toggle("active");
+  body.classList.toggle("active");
 };
 window.onclick = function (event) {
   if (event.target == menu) {
@@ -19,12 +19,31 @@ window.onclick = function (event) {
   }
 };
 
-const header = document.getElementById('header');
+const header = document.getElementById("header");
 if (header)
-  window.addEventListener('scroll', () => {
+  window.addEventListener("scroll", () => {
     if (document.documentElement.scrollTop > 400) {
-      header.classList.add('sticky');
+      header.classList.add("sticky");
     } else {
-      header.classList.remove('sticky');
+      header.classList.remove("sticky");
     }
   });
+
+const inputPassword = document.querySelectorAll(".input--password");
+if (inputPassword) {
+  inputPassword.forEach((item) => {
+    let visible = false;
+    button = item.querySelector(".input__icon-right");
+    input = item.querySelector("input");
+    button.onclick = () => {
+      visible = !visible;
+      if (visible) {
+        input.setAttribute("type", "text");
+        item.classList.add("_visable");
+      } else {
+        input.setAttribute("type", "password");
+        item.classList.remove("_visable");
+      }
+    };
+  });
+}
